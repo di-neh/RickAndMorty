f733836a496a0a2c7599ee3a5accb915e445a1c8
dineh <seva25178@gmail.com>
2024-06-06 19:52:01 +0300

    finish

--- rim/src/App.vue
+++ rim/src/App.vue
@@ -1,18 +1,162 @@
 <template>
-  <h1>187</h1>
+  <div class="wrapper">
+    <div class="head">
+      <h1>Rick and Morty</h1>
+      <div class="filters">
+        <input v-model="filters.name" placeholder="Фильтрация по имени">
+        <select v-model="filters.status">
+          <option value="">Все статусы</option>
+          <option value="Alive">Alive</option>
+          <option value="Dead">Dead</option>
+          <option value="unknown">Unknown</option>
+        </select>
+        <button @click="applyFilters">Принять</button>
+      </div>
+    </div>
+    <div class="main">
+      <CharCard 
+        v-for="card in cards" 
+        :key="card.id" 
+        :name="card.name" 
+        :status="card.status" 
+        :species="card.species" 
+        :image="card.image" 
+        :origin="card.origin.name" 
+        :location="card.location.name"
+      />
+    </div>
+    <div class="pagination">
+      <button @click="prevPage" :disabled="currentPage === 1">Previous</button>
+      <span>Page {{ currentPage }} of {{ totalPages }}</span>
+      <button @click="nextPage" :disabled="currentPage === totalPages">Next</button>
+    </div>
+  </div>
 </template>
 
 <script>
+import CharCard from './components/CharCard.vue';
+import { ref, onMounted, reactive } from 'vue';
+import axios from 'axios';
 
+export default {
+  name: 'App',
+  components: {
+    CharCard,
+  },
+  setup() {
+    const cards = ref([]);
+    const currentPage = ref(1);
+    const totalPages = ref(1);
+    const filters = reactive({
+      name: '',
+      status: ''
+    });
+
+    const fetchData = async (page = 1) => {
+      try {
+        const response = await axios.get('https://rickandmortyapi.com/api/character/', {
+          params: {
+            page,
+            name: filters.name,
+            status: filters.status
+          }
+        });
+        if (response.data && response.data.results && response.data.info) {
+          cards.value = response.data.results;
+          totalPages.value = response.data.info.pages;
+          console.log(cards.value);
+        } else {
+          console.error('Unexpected response structure:', response.data);
+        }
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    const nextPage = () => {
+      if (currentPage.value < totalPages.value) {
+        currentPage.value += 1;
+        fetchData(currentPage.value);
+      }
+    };
+
+    const prevPage = () => {
+      if (currentPage.value > 1) {
+        currentPage.value -= 1;
+        fetchData(currentPage.value);
+      }
+    };
+
+    const applyFilters = () => {
+      currentPage.value = 1; // Reset to the first page
+      fetchData();
+    };
+
+    onMounted(() => {
+      fetchData();
+    });
+
+    return {
+      cards,
+      currentPage,
+      totalPages,
+      nextPage,
+      prevPage,
+      filters,
+      applyFilters
+    };
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  font-family: Arial, Helvetica, sans-serif;
+}
+* {
+  margin: 0;
+  padding: 0;
+}
+.wrapper {
+  display: flex;
+  flex-direction: column;
+}
+.head {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding: 3%;
+  font-size: 30px;
+}
+.filters {
+  display: flex;
+  justify-content: center;
+  margin-top: 10px;
+  margin-bottom: 20px;
+}
+.filters input,
+.filters select {
+  margin: 0 10px;
+  padding: 5px;
+}
+.filters button {
+  padding: 5px 10px;
+  cursor: pointer;
+}
+.main {
+  display: flex;
+  flex-wrap: wrap;
+  background: rgb(53, 52, 62);
+  color: whitesmoke;
+  padding: 5%;
+}
+.pagination {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 10px;
+}
+.pagination button {
+  margin: 0 5px;
+  padding: 5px 10px;
+  cursor: pointer;
 }
 </style>
---
+++ rim/src/components/CharCard.vue
@@ -0,0 +1,73 @@
+<template>
+    <div class="card">
+        <div class="picture">
+            <img :src="image" height="220px" width="229px">
+        </div>
+        <div class="conteiner">
+            <div class="box">
+                <h1>
+                    {{name}}
+                </h1>
+                <h2>{{ status }} - {{ species }}</h2>
+            </div>
+
+            <div class="box">
+                <p>Last known location</p>
+                <div> {{ location }}</div>
+            </div>
+
+            <div class="box">
+                <p>First seen in:</p>
+                <div>{{ origin }}</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent ({
+    name: 'CharCard',
+    props: {
+        name: String,
+        status: String,
+        species: String,
+        image: String,
+        location: String,
+        origin: String
+    }
+})
+</script>
+
+<style scoped>
+    .card{
+        display: flex;
+        background: rgb(81, 80, 89);
+        width: 600px;
+        height: 220px;
+        border-radius: 10px;
+        flex: 1 1 calc(33.333% - 10px);
+        max-width: calc(33.333% - 10px);
+        margin: 5px;
+    }
+    h1 {
+        font-size: 30px;
+    }
+    h2 {
+        font-size: 15px;
+    }
+    p {
+        color: grey;
+        margin-bottom: 5px;
+    }
+    .conteiner {
+        padding: 15px;
+    }
+    .box {
+        margin-bottom: 15px;
+    }
+    .picture img {
+    border-radius: 10px 0 0 10px;
+}
+</style>
